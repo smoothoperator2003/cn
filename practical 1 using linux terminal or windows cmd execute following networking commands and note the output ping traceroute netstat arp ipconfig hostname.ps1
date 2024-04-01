9532dc0b87cf7c8c1e488493c4c5554cd46e6cdd
Smooth Operator <143164739+smoothoperator2003@users.noreply.github.com>
2024-04-01 15:21:31 +0530

    Add files via upload

---
+++ practical 1 using linux terminal or windows cmd execute following networking commands and note the output ping traceroute netstat arp ipconfig hostname.ps1
@@ -0,0 +1,13 @@
+# AIM:-Using, linux-terminal or Windows-cmd, execute following networking commands and note the output: ping, traceroute, netstat, arp, ipconfig, hostname.
+
+ping google.com
+
+traceroute 8.8.8.8.
+
+netstat
+
+arp -a
+ 
+ipconfig
+
+hostname
